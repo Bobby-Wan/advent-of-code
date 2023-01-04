2cecbb4a4d27d2339741e42306fd1ac348bcf797
Bozhidar Atanasov <bozhidara@vmware.com>
2023-01-04 02:56:59 +0200

    AoC problem 2 part 2

--- p2.rs
+++ p2.rs
@@ -6,7 +6,7 @@ use std::io::prelude::*;
 use std::io::{self, BufRead};
 
 
-fn main() {
+fn part1() {
     let file = std::fs::File::open("input2").unwrap();
     let lines = io::BufReader::new(file).lines();
 
@@ -31,6 +31,36 @@ fn main() {
     println!("Total score is {}", total_score);
 }
 
+fn part2() {
+    let file = std::fs::File::open("input2").unwrap();
+    let lines = io::BufReader::new(file).lines();
+
+    let mut total_score = 0;
+
+    for line in lines {
+        if let Ok(validated_line) = line {
+            println!("{}", validated_line);
+            let mut parts = validated_line.split(" ");
+            let opponent_move = parts.next();
+            let strategy = parts.next();
+
+            match (opponent_move, strategy) {
+                (Some(opponent), Some(strat)) => { 
+                    let my_move = choose_move(opponent, strat);
+                    total_score += score_move(opponent, my_move.as_str());
+                }
+                _ => { println!("Could not extract moves"); return; }
+            };
+        }
+    }
+    
+    println!("Total score is {}", total_score);
+}
+
+fn main() {
+    part2();
+}
+
 fn score_move(op: &str, me: &str) -> i32 {
     match (op, me) {
         ("A", "X") => 1+3,
@@ -43,5 +73,20 @@ fn score_move(op: &str, me: &str) -> i32 {
         ("C", "X") => 1+6,
         ("C", "Y") => 2+0,
         (_, _) => { println!("Unexpected moves to score. Returning -1"); -1 }
+    }
+}
+
+fn choose_move(op: &str, strategy: &str) -> String {
+    match (op, strategy) {
+        ("A", "X") => String::from("Z"),
+        ("B", "Y") => String::from("Y"),
+        ("C", "Z") => String::from("X"),
+        ("A", "Y") => String::from("X"),
+        ("A", "Z") => String::from("Y"),
+        ("B", "X") => String::from("X"),
+        ("B", "Z") => String::from("Z"),
+        ("C", "X") => String::from("Y"),
+        ("C", "Y") => String::from("Z"),
+        (_, _) => { println!("Unexpected strategy. Returning empty string"); String::from("") }
     }
 }
